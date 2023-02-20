274124a7dd0a5191effd9758b986eeaab0d0aef0
PhungDTrung <125850124+PhungDTrung@users.noreply.github.com>
2023-02-20 16:23:33 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'About me',
-  domain: 'niviki.com',
+  domain: 'trungphung.vercel.app',
   author: 'Trung Phung',
 
   // open graph metadata (optional)
